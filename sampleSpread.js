efa490bc177751b41283b0d02ddcf82798786e9c
Mahesh <itstribe.mahi@gmail.com>
2022-08-31 16:23:48 +0530

    Spread Parameter

---
+++ sampleSpread.js
@@ -0,0 +1,36 @@
+// // normal spread operator
+// let num1=[234,45,4356,4536]
+// let num2=['RRR','KGF','SVP',234]
+// let num3=[2.5,5.6,'mahesh','randy']
+// let result=[...num1, ...num2, ...num3]; /// spread operator
+// console.log(result);
+
+
+
+//  for array  objects
+// let obj1=[{name:"mahesh",age:26, "add":"Jagtial"}]
+// let obj2=[{'name':"Ramya","age":24, "add":"Karimanagar"}]
+// let obj3=[{'name':"nikhitha","age":23, "add":"hyd"}]
+// let res=[...obj2,...obj3]// here we can take all objects or only requirement objects.
+// console.log(res);
+
+let obj10={name:"mahesh",age:26, "add":"Jagtial"}
+let obj20={'name':"Ramya","age":24, "add":"Karimanagar"}
+let obj30={'name':"nikhitha","age":23, "add":"hyd"}
+let res2=[...obj20,...obj30];// here we can take all objects or only requirement objects.
+console.log(res2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ spread2.js
@@ -0,0 +1,32 @@
+
+// const myVehicle = {
+//     brand: 'Ford',
+//     model: 'Mustang',
+//     color: 'red'
+//   }
+  
+//   const updateMyVehicle = {
+//     type: 'car',
+//     year: 2021, 
+//     color: 'yellow'
+//   }
+//   const updateMyVehicle2 = {
+//     type: 'car',
+//     year: 2021, 
+//     color: 'white',
+//     addr:'Jagtial'
+//   }
+//   const myUpdatedVehicle = {...myVehicle, ...updateMyVehicle,...updateMyVehicle2}
+  
+//   console.log(myUpdatedVehicle);
+  
+
+
+
+
+
+
+
+
+
+
